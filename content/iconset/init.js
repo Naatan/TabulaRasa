75dc405b8575fe8dae7558886eace9d6303fbb19
Nathan Rijksen <n.rijksen@gmail.com>
2015-07-27 00:09:16 -0400

    Primarily cleaned up trees, menu's and toolbars

--- content/iconset/init.js
+++ content/iconset/init.js
@@ -2,4 +2,5 @@ if (koSkin.shouldFlushCaches)
 {
     Services.prefs.setCharPref("general.skins.selectedSkin", "tabularasa");
     ko.prefs.setString("iconset-base-color", "#C8C8C8");
+    ko.prefs.setString("iconset-selected-color", "#FFFFFF");
 }
